25d4b179c078309bf50f53a900654d7e0bb71fba
youba <lakhmesyouba@hotmail.fr>
2023-05-16 01:42:56 +0200

    organisation du code des deux module app et felm-serie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,43 +7,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
-import { FilmComponent } from './film-serie/components/film/film.component';
-import { ListFilmComponent } from './film-serie/components/list-film/list-film.component';
-import { ListNouveauFilmComponent } from './film-serie/components/list-nouveau-film/list-nouveau-film.component';
-import { DetailsFilmComponent } from './film-serie/components/details-film/details-film.component';
-import { TimePipe } from './film-serie/pipes/time.pipe';
 import { MaterialAngularModule } from './material-modules/material.module';
-import { ListSerieComponent } from './film-serie/components/list-serie/list-serie.component';
-import { DetailsSerieComponent } from './film-serie/components/details-serie/details-serie.component';
-import { SerieComponent } from './film-serie/components/serie/serie.component';
-import { StringPipe } from './film-serie/pipes/string.pipe';
-import { GenreComponent } from './film-serie/components/genre/genre.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FilmSerieModule } from './film-serie/film-serie.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FilmComponent,
-    ListFilmComponent,
-    ListNouveauFilmComponent,
-    DetailsFilmComponent,
-    TimePipe,
-    ListSerieComponent,
-    DetailsSerieComponent,
-    SerieComponent,
-    StringPipe,
-    GenreComponent,
     PageNotFoundComponent,
   ],
   imports: [
+    FilmSerieModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
     HttpClientModule,
     MaterialAngularModule
+  ],
+  exports:[
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/film-serie/film-serie.module.ts
+++ src/app/film-serie/film-serie.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailsFilmComponent } from './components/details-film/details-film.component';
+import { DetailsSerieComponent } from './components/details-serie/details-serie.component';
+import { FilmComponent } from './components/film/film.component';
+import { GenreComponent } from './components/genre/genre.component';
+import { ListFilmComponent } from './components/list-film/list-film.component';
+import { ListNouveauFilmComponent } from './components/list-nouveau-film/list-nouveau-film.component';
+import { ListSerieComponent } from './components/list-serie/list-serie.component';
+import { SerieComponent } from './components/serie/serie.component';
 
-
+import { AppRoutingModule } from '../app-routing.module';
+import { StringPipe } from './pipes/string.pipe';
+import { TimePipe } from './pipes/time.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FilmComponent,
+    ListFilmComponent,
+    ListNouveauFilmComponent,
+    DetailsFilmComponent,
+    ListSerieComponent,
+    DetailsSerieComponent,
+    SerieComponent,
+    GenreComponent,
+    TimePipe,
+    StringPipe,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class FilmSerieModule { }
